b293062ac9ae00e4b7d1d226c26685c76628bca2
IYStoychev21 <IYStoychev21@codingburgas.bg>
2024-07-13 01:55:35 +0300

    Add profile avatar

--- src/components/RoomCard.tsx
+++ src/components/RoomCard.tsx
@@ -2,7 +2,7 @@
 export default function RoomCard({ title } : { title: string }) {
     return (
         <>
-            <div className="text-text-50 flex items-center justify-center w-full overflow-hidden h-[200px] cursor-pointer bg-red-900 rounded-xl relative before:absolute before:w-full before:h-full before:bg-gradient-to-t before:from-[#000] before:to-[#00000000] before:rounded-xl hover:scale-105 duration-100">
+            <div className="text-text-50 flex items-center justify-center w-full overflow-hidden h-[200px] active:scale-100 cursor-pointer bg-red-900 rounded-xl relative before:absolute before:w-full before:h-full before:bg-gradient-to-t before:from-[#000] before:to-[#00000000] before:rounded-xl hover:scale-105 duration-100">
                 <img className="w-full" src={`https://api.dicebear.com/9.x/shapes/svg?seed=${title}`} alt="" />
                 <h1 className="z-10 absolute bottom-5 left-5 font-bold text-2xl">{title}</h1>
             </div>
--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,18 +1,21 @@
 import RoomCard from "@/components/RoomCard"
+import ProfileAvatar from "@/components/ProfileAvatar"
 
 import RoomAnimation from "@/components/animations/RoomAnimation"
 
 export default function Dashboard() {
     return (
         <>
-            <div className="bg-background-950 min-h-screen min-w-screen flex">
+            <div className="bg-background-950 min-h-screen min-w-screen flex relative">
+                <ProfileAvatar className="right-5 top-5 absolute" name="Ivan Stoychev" role="student"/>
+
                 <div className="w-1/6 min-h-full bg-background-900 border-text-200 border-r-[1px]"></div>
                 <div className="w-full flex flex-col items-center gap-8">
-                    <div className=" w-[70%] h-[200px] bg-accent-800 rounded-md mt-20 overflow-hidden max-lg:hidden">
+                    <div className=" w-[70%] h-[200px] bg-secondary-800 rounded-md mt-28 overflow-hidden max-lg:hidden">
                         <RoomAnimation />
                     </div>
 
-                    <div className="w-[80%] pt-10">
+                    <div className="w-[80%] pt-10 max-lg:mt-16">
                         <h1 className="text-text-100 font-bold text-3xl">Welcome back, Pesho</h1>
                         <p className="text-text-300 text-lg">Your classrooms</p>
                     </div>
