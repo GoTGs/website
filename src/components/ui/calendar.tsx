5a0a6968db4340fa1be938f31b1dc728f20b916c
IYStoychev21 <IYStoychev21@codingburgas.bg>
2024-07-24 00:35:02 +0300

    Fix ts errors in calendar.tsx

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -58,7 +58,9 @@ function Calendar({
         ...classNames,
       }}
       components={{
+        // @ts-ignore
         IconLeft: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
+        // @ts-ignore
         IconRight: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
       }}
       {...props}
